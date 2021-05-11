6acccab64bc0703a1decb6c7a99427d5d5668867
burakatalay06 <63429097+burakatalay06@users.noreply.github.com>
2021-05-11 05:59:42 +0300

    Add files via upload

---
+++ ten_minutes/consts.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+
+class colors {
+  dynamic yellow = Color(0XFFFEDB1F);
+  dynamic grey = Color(0XFFB8B8B8);
+  dynamic brown = Color(0XFF34322B);
+  dynamic haki_green = Color(0XFF32342B);
+  dynamic orange_google = Color(0XFFE8674A);
+  dynamic grey_2 = Color(0XFF4A4A4A);
+}
